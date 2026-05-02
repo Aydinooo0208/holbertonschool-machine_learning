3ba22d3a48f368ad4fe0c4f25647f2ce3fbd4a38
Aydinooo0208 <13758@holbertonstudents.com>
2026-05-02 21:52:43 +0400

    Update 5-across_the_planes.py

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -3,7 +3,7 @@
 
 
 def add_matrices2D(mat1, mat2):
-    """Returns a new 2D matrix with element-wise sum, or None if shapes differ"""
+    """Returns element-wise sum or None if shapes differ"""
     if len(mat1) != len(mat2) or len(mat1[0]) != len(mat2[0]):
         return None
     return [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))]
