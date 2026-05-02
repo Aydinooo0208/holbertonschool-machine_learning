5c0c6d12fc897d3f6d5366bc88e1df28d2b90865
Aydinooo0208 <13758@holbertonstudents.com>
2026-05-02 21:50:16 +0400

    Create 4-line_up.py

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Function that adds two arrays element-wise"""
+
+
+def add_arrays(arr1, arr2):
+    """Returns a new list with element-wise sum, or None if different shapes"""
+    if len(arr1) != len(arr2):
+        return None
+    return [arr1[i] + arr2[i] for i in range(len(arr1))]
