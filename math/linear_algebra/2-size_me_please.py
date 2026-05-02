40ebf95c65971b378f4de044551cc88e519b8a82
Aydinoo <13758@holbertonstudents.com>
2026-05-02 17:45:23 +0000

    si me pls2

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
+"""Function that calculates the shape of a matrix"""
+
 
 def matrix_shape(matrix):
+    """Returns the shape of a matrix as a list of integers"""
     shape = []
     current = matrix
     while isinstance(current, list):
         shape.append(len(current))
         current = current[0]
     return shape
-
