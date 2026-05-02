40e19020ea2763306e381e5fb4e266943ad76bd2
Aydinoo <13758@holbertonstudents.com>
2026-05-02 18:22:35 +0000

    western exchange

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Function that transposes a numpy ndarray"""
+
+
+def np_transpose(matrix):
+    """Returns the transpose of a numpy ndarray as a new ndarray"""
+    return matrix.T.copy()
