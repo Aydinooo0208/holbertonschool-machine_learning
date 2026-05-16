cd5d67b577811e2fd3ff267c5967d4df89ce6bea
Aydinooo0208 <13758@holbertonstudents.com>
2026-05-16 21:48:26 +0400

    Update exponential.py

--- math/probability/exponential.py
+++ math/probability/exponential.py
@@ -5,6 +5,8 @@
 class Exponential:
     """Represents an Exponential distribution"""
 
+    e = 2.7182818285
+
     def __init__(self, data=None, lambtha=1.):
         """
         Initializes Exponential distribution
@@ -23,3 +25,18 @@ class Exponential:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             self.lambtha = float(lambtha)
+
+    def pdf(self, x):
+        """
+        Calculates the value of the PDF for a given time period
+
+        Args:
+            x: the time period
+
+        Returns:
+            PDF value for x, or 0 if x is out of range
+        """
+        if x < 0:
+            return 0
+
+        return self.lambtha * (self.e ** (-self.lambtha * x))
