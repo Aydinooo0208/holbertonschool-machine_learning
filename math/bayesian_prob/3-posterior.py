c0edb89bfdc499cd31fa0da043e4ed92d6bf8e93
Aydinooo0208 <13758@holbertonstudents.com>
2026-05-16 23:31:45 +0400

    Create 3-posterior.py

---
+++ math/bayesian_prob/3-posterior.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""Module for calculating posterior probability"""
+import numpy as np
+
+
+def posterior(x, n, P, Pr):
+    """
+    Calculates the posterior probability for the various hypothetical
+    probabilities of developing severe side effects given the data
+
+    Args:
+        x: number of patients that develop severe side effects
+        n: total number of patients observed
+        P: 1D numpy.ndarray of hypothetical probabilities
+        Pr: 1D numpy.ndarray of prior beliefs of P
+
+    Returns:
+        1D numpy.ndarray of posterior probability of each probability in P
+    """
+    if not isinstance(n, int) or n <= 0:
+        raise ValueError("n must be a positive integer")
+    if not isinstance(x, int) or x < 0:
+        raise ValueError(
+            "x must be an integer that is greater than or equal to 0"
+        )
+    if x > n:
+        raise ValueError("x cannot be greater than n")
+    if not isinstance(P, np.ndarray) or P.ndim != 1:
+        raise TypeError("P must be a 1D numpy.ndarray")
+    if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
+        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+    if np.any((P < 0) | (P > 1)):
+        raise ValueError("All values in P must be in the range [0, 1]")
+    if np.any((Pr < 0) | (Pr > 1)):
+        raise ValueError("All values in Pr must be in the range [0, 1]")
+    if not np.isclose(Pr.sum(), 1):
+        raise ValueError("Pr must sum to 1")
+
+    k_factorial = 1
+    for i in range(1, x + 1):
+        k_factorial *= i
+
+    n_factorial = 1
+    for i in range(1, n + 1):
+        n_factorial *= i
+
+    nk_factorial = 1
+    for i in range(1, n - x + 1):
+        nk_factorial *= i
+
+    combination = n_factorial / (k_factorial * nk_factorial)
+    likelihood = combination * (P ** x) * ((1 - P) ** (n - x))
+
+    intersection = likelihood * Pr
+    marginal = np.sum(intersection)
+
+    return intersection / marginal
